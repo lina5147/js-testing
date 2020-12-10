4cdb2b86b684c9538df5e91059bdcb20a87f90b9
Lina Do <l.do5147@gmail.com>
2020-12-10 14:13:35 -0800

    wrote panagram function and tests

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,31 @@
-const isPangram = function(text) {
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
+const isPangram = function(text) {
+  
+  const textLowerCase = text.toLowerCase()
+  
+  const letterCount = {};
+
+  // const letters = text.split('');
+  // letters.forEach( (letter) => {
+  //   if (letterCount[letter]) {
+  //     letterCount[letter] += 1;
+  //   } else {
+  //     letterCount[letter] = 1
+  //   }
+  // });
+
+  for (const letter of textLowerCase) {
+    letterCount[letter] = true;
+  }
+
+  for (const letter of ALPHABET) {
+    if (!letterCount[letter]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 module.exports = isPangram;
