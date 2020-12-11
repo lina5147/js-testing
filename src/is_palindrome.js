0cbd9afcd414d56f5a9e36af8d661c545cd1e9f6
Lina Do <l.do5147@gmail.com>
2020-12-10 16:17:12 -0800

    created is_palindrome function and tests

--- src/is_palindrome.js
+++ src/is_palindrome.js
@@ -1,5 +1,25 @@
 const isPalindrome = function (text) {
 
+	if (text.length <= 1) return true;
+
+	const newText = text.replace(/[^A-Z0-9]/ig, '').toLowerCase();
+
+	console.log(newText);
+
+	let left = 0;
+	let right = newText.length - 1;
+
+	while (left < right) {
+		if (newText[left] !== newText[right]) {
+			return false;
+		}
+
+		left++;
+		right--;
+	}
+
+	return true;
+
 };
 
 module.exports = isPalindrome;
---
+++ test/is_palindrome.test.js
@@ -0,0 +1,53 @@
+const isPalindrome = require('../src/is_palindrome');
+const isPangram = require('../src/is_palindrome');
+
+describe('isPangram()', () => {
+
+  test('isPangram() is defined', () => {
+    expect(isPalindrome).toBeDefined();
+  });
+
+  test('works for 1-word palindromes', () => {
+    
+    expect(isPalindrome('a')).toEqual(true);
+    expect(isPalindrome('racecar')).toEqual(true);
+    expect(isPalindrome('poop')).toEqual(true);
+
+  });
+
+  test('will return false for non-palindrome 1-word strings', () => {
+    
+    expect(isPalindrome('hello')).toEqual(false);
+    expect(isPalindrome('race')).toEqual(false);
+    expect(isPalindrome('candle')).toEqual(false);
+
+  });
+
+  test('returns true for an empty string', () => {
+    
+    expect(isPalindrome('')).toEqual(true);
+
+  });
+
+  test('will return true for long phrases with spaces and puntuation', () => {
+    
+    // Arrange
+    const palindromeWord = 'A man, a plan, a canal: Panama';
+
+    // Act-Assert
+    expect(isPalindrome(palindromeWord)).toEqual(true);
+
+  });
+
+  test('will return false for non-palindrom long phrases with spaces and puntuation', () => {
+    
+    // Arrange
+    const palindromeWord = 'A man, a plan, a canal: Panam';
+
+    // Act-Assert
+    expect(isPalindrome(palindromeWord)).toEqual(false);
+
+  });
+
+
+});
